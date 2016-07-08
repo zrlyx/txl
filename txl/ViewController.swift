f7a5e6793b5badd011aaa07e04447ff7939df04f
zrlyx <20141105042hyb@admindeimac-007.local>
2016-07-08 09:58:48 +0800

    Initial Commit

---
+++ txl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  txl
+//
+//  Created by  20141105042hyb on 16/7/8.
+//  Copyright © 2016年 20141105042HYB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
